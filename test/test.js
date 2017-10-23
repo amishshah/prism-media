326e06e593c3f1841a10afdcf2f2051af93fe66f
Amish Shah <amishshah.2k@gmail.com>
2017-10-23 22:22:12 +0100

    plsss

--- test/test.js
+++ test/test.js
@@ -17,10 +17,16 @@ test('FFmpeg transcoder to PCM is sane', async done => {
         '-ac', '2',
       ],
     }));
-  
+
   const chunks = await streamToBuffer(output);
   const file = await readFile('./test/audio/speech_orig.pcm');
-  expect(file == chunks).toEqual(true);
+  let x = [];
+  for (let i = 0; i < file.length; i++) {
+    if (chunks[i] !== file[i]) {
+      console.log(i, chunks[i], file[i])
+    }
+  }
+  expect(file.equals(chunks)).toEqual(true);
 
   done();
 });
@@ -30,15 +36,15 @@ test('OggOpus demuxer is sane', async done => {
   const output = fs.createReadStream('./test/audio/speech_orig.ogg').pipe(new prism.demuxers.OggOpus());
   const chunks = await streamToBuffer(output);
   const file = await readFile('./test/audio/speech_orig.opusdump');
-  expect(file == chunks).toEqual(true);
+  expect(file.equals(chunks)).toEqual(true);
   done();
 });
 
 function streamToBuffer(stream) {
   return new Promise((resolve, reject) => {
-    let chunks = '';
-    stream.on('data', chunk => chunks += chunk);
+    let chunks = [];
+    stream.on('data', chunk => chunks.push(chunk));
     stream.on('error', reject);
-    stream.on('end', () => resolve(chunks));
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
   });
 }
