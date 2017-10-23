b93c88bed2b9e52142d6cf0d52dea35fbca81e12
Amish Shah <amishshah.2k@gmail.com>
2017-10-23 21:52:42 +0100

    plsss

--- test/test.js
+++ test/test.js
@@ -20,10 +20,14 @@ test('FFmpeg transcoder to PCM is sane', async done => {
   
   const chunks = await streamToBuffer(output);
   const file = await readFile('./test/audio/speech_orig.pcm');
-  console.log(file.length, chunks.length);
-  console.log(file);
-  console.log(chunks);
   expect(chunks.equals(file)).toEqual(true);
+
+  for (let i = 0; i < 2074848; i++) {
+    if (chunks.readUInt8(i) !== file.readUInt8(i)) {
+      console.log(i, chunks.readUint8(i), file.readUInt8(i))
+    }
+  }
+
   done();
 });
 
