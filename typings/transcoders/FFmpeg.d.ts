59f9a3f570f5f6f2c16117c1fe44ea9e5b6dc30b
Will Nelson <nelson.will@live.com>
2017-10-31 18:16:54 -0700

    add ffmpeg options

--- typings/transcoders/FFmpeg.d.ts
+++ typings/transcoders/FFmpeg.d.ts
@@ -1,7 +1,9 @@
 import { ChildProcess } from 'child_process';
 import { Duplex } from 'stream';
 
-export interface FFmpegOptions {}
+export interface FFmpegOptions {
+  args?: string[];
+}
 
 export default class FFmpegTransform extends Duplex {
   public process: ChildProcess;
