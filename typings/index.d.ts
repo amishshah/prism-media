ac4e610760d999d76a74504d9582179f4d9450be
Hackzzila <admin@hackzzila.com>
2018-06-26 13:15:16 -0500

    add missing semi

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,7 +2,7 @@ import opus from './transcoders/Opus';
 import FFmpeg from './transcoders/FFmpeg';
 import OggOpusDemuxer from './demuxers/OggOpus';
 import WebmOpusDemuxer from './demuxers/WebmOpus';
-import WebmVorbisDemuxer from './demuxers/WebmVorbis'
+import WebmVorbisDemuxer from './demuxers/WebmVorbis';
 
 export {
   VolumeTransformer16LE,
