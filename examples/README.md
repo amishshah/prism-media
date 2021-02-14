c8a573dc78b46257d854a7f5c30b2505e7b7d1e4
Amish Shah <amishshah.2k@gmail.com>
2021-02-14 12:35:32 +0000

    chore: nuke repo

--- examples/README.md
+++
@@ -1,4 +0,0 @@
-## Examples
-
-Examples are also available within the [official documentation](https://hydrabolt.me/prism-media/?api) and they
-may be more useful than the examples listed here if you're looking for how to use a single feature.
--- test/vorbis.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-const prism = require('../');
-
-test('vorbis.WebmDemuxer available', () => {
-  expect(prism.vorbis.WebmDemuxer).toBeTruthy();
-  expect(prism.vorbis.WebmDemuxer.TOO_SHORT).toBeTruthy();
-  expect(prism.vorbis.WebmDemuxer.TAGS).toBeTruthy();
-});
